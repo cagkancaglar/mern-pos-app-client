e808214eb0d4aded0a8e209256b604a4bef4c9b3
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-07-10 01:17:24 +0300

    cart-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" index element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,65 @@
+import { Table, Card, Button } from "antd";
+import Header from "../components/Header";
+
+const CartPage = () => {
+  const dataSource = [
+    {
+      key: "1",
+      name: "Mike",
+      age: 32,
+      address: "10 Downing Street",
+    },
+    {
+      key: "2",
+      name: "John",
+      age: 42,
+      address: "10 Downing Street",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+  ];
+
+  return (
+    <div>
+      <Header />
+      <div className="px-6">
+        <Table dataSource={dataSource} columns={columns} bordered pagination={false}/>
+        <div className="flex justify-end mt-4">
+          <Card className="w-72">
+            <div className="flex justify-between">
+              <span className="capitalize">subtotal</span>
+              <span>1000.00 $</span>
+            </div>
+            <div className="flex justify-between my-2">
+              <span className="uppercase">kdv</span>
+              <span className="text-red-600">180.00 $</span>
+            </div>
+            <div className="flex justify-between">
+              <span className="capitalize font-semibold text-lg">subtotal</span>
+              <span className="font-semibold text-lg">550.00 $</span>
+            </div>
+            <Button type="primary" className="mt-4 w-full" size="large">Create Order</Button>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartPage;
