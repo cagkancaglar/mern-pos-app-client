09b22be121b9895071c72720061baad0b7013404
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-07-09 20:44:44 +0300

    products

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import Header from "./components/Header";
 import Categories from "./components/Categories";
+import Products from "./components/Products";
 
 function App() {
   return (
     <>
       <Header />
-      <div className="home px-6 flex justify-between items-center">
-        <div className="categories">
+      <div className="home px-6 flex justify-between gap-10">
+        <div className="categories flex-1 max-h-[calc(100vh-_-112px)] overflow-auto">
         <Categories />
         </div>
-        <div className="products">
-          products
+        <div className="products flex-[8]">
+          <Products />
         </div>
         <div className="cart-totals">
           cart totals
--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -2,8 +2,44 @@
 const Categories = () => {
   return (
     <ul className="flex gap-4 flex-col">
-        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all">
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
             <span className="capitalize">all</span>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+            <span className="capitalize">food</span>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+            <span className="capitalize">drink</span>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+            <span className="capitalize">drink</span>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+            <span className="capitalize">drink</span>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+            <span className="capitalize">drink</span>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+            <span className="capitalize">drink</span>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+            <span className="capitalize">drink</span>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+            <span className="capitalize">drink</span>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+            <span className="capitalize">drink</span>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+            <span className="capitalize">drink</span>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+            <span className="capitalize">drink</span>
+        </li>
+        <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+            <span className="capitalize">drink</span>
         </li>
     </ul>
   )
