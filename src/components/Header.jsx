4f8b54af6fe5878c4be141b635dd938eed1b0db8
Çağkan Çağlar <cagkancaglar@hotmail.com>
2023-07-12 00:47:35 +0300

    header-page-link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,15 +8,16 @@ import {
   BarChartOutlined,
   LogoutOutlined,
 } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="border-b mb-6">
       <header className="py-4 px-6 flex justify-between items-center gap-10">
         <div className="logo">
-          <a href="/">
+          <Link to="/">
             <h2 className="text-2xl font-bold md:text-4xl">LOGO</h2>
-          </a>
+          </Link>
         </div>
         <div className="header-search w-full ">
           <Input
@@ -27,59 +28,59 @@ function Header() {
           />
         </div>
         <div className="menu-links flex justify-center items-center gap-x-7 fixed md:static bottom-0 md:w-auto w-full bg-white md:bg-transparent left-0 border-t md:border-t-0 p-4 md:p-0 z-50">
-          <a
-            href="/"
+          <Link
+            to="/"
             className="menu-link flex flex-col items-center justify-center gap-y-1 hover:text-[#40a9ff] transition-all"
           >
             <HomeOutlined className="text-md md:text-2xl " />
             <span className="text-sm md:text-md capitalize">home</span>
-          </a>
+          </Link>
           <Badge count={5} offset={[0, 6]} className="hidden md:flex">
-            <a
-              href="/"
+            <Link
+              to="/cart"
               className="menu-link flex flex-col items-center justify-center gap-y-1 hover:text-[#40a9ff] transition-all"
             >
               <ShoppingCartOutlined className="text-md md:text-2xl " />
               <span className="text-sm md:text-md capitalize">cart</span>
-            </a>
+            </Link>
           </Badge>
-          <a
-            href="/"
+          <Link
+            to="/"
             className="menu-link flex flex-col items-center justify-center gap-y-1 hover:text-[#40a9ff] transition-all"
           >
             <CopyOutlined className="text-md md:text-2xl " />
             <span className="text-sm md:text-md capitalize">invoices</span>
-          </a>
-          <a
-            href="/"
+          </Link>
+          <Link
+            to="/"
             className="menu-link flex flex-col items-center justify-center gap-y-1 hover:text-[#40a9ff] transition-all"
           >
             <UserOutlined className="text-md md:text-2xl " />
             <span className="text-sm md:text-md capitalize">customers</span>
-          </a>
-          <a
-            href="/"
+          </Link>
+          <Link
+            to="/"
             className="menu-link flex flex-col items-center justify-center gap-y-1 hover:text-[#40a9ff] transition-all"
           >
             <BarChartOutlined className="text-md md:text-2xl " />
             <span className="text-sm md:text-md capitalize">statistics</span>
-          </a>
-          <a
-            href="/"
+          </Link>
+          <Link
+            to="/"
             className="menu-link flex flex-col items-center justify-center gap-y-1 hover:text-[#40a9ff] transition-all"
           >
             <LogoutOutlined className="text-md md:text-2xl " />
             <span className="text-sm md:text-md capitalize">logout</span>
-          </a>
+          </Link>
         </div>
         <Badge count={5} offset={[0, 6]} className="md:hidden">
-          <a
-            href="/"
+          <Link
+            to="/"
             className="menu-link flex flex-col items-center justify-center gap-y-1 hover:text-[#40a9ff] transition-all"
           >
             <ShoppingCartOutlined className="text-2xl" />
             <span className="text-sm md:text-md capitalize">cart</span>
-          </a>
+          </Link>
         </Badge>
       </header>
     </div>
